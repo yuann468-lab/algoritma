b18cafa8994fc3f795eb907619e755ec2f24ff4e
yuann468-lab <yuann468@gmail.com>
2026-08-05 08:32:27 +0700

    Add files via upload

---
+++ dua.py
@@ -0,0 +1,20 @@
+# Program Pertamaku 
+# Nama: [caca dan yuan]
+# Kelas: X RPL
+
+
+print ("----------------------------------------")
+print ("       SELAMAT DATANG DI PYTHON!")
+print ("----------------------------------------")
+print ()
+print ("Program ini dibuat oleh:", "[caca dan yuan]")
+print ("Sekolah: SMK TJP Tuban")
+print ("Tahun  :", 2024)
+
+print (10 + 5)
+print (20 - 8)
+print (4 * 8)
+print (15 / 4)
+print (15 // 4)
+print (15 % 4)
+print (2 ** 8)
