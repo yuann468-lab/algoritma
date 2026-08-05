a4dcae3d8c8b3745af540a93e92071d726cea8df
yuann468-lab <yuann468@gmail.com>
2026-08-05 12:40:39 +0700

    Add files via upload

---
+++ Bab3.py
@@ -0,0 +1,17 @@
+nama = "Yuanrpl"
+print ("nama : ", nama)
+umur = 15
+print ("umur : ", umur)
+tinggi = 165
+print ("tinggi : ", tinggi)
+Lulus = True
+print ("Lulus : ", Lulus) 
+
+produk="sepatu lari"
+print("nama produk:", produk)
+stok=17
+print("stok:", stok)
+harga="5.000.000.000"
+print("harga :", harga)
+tersedia_ditoko ="true"
+print("tersedia_ditoko:",tersedia_ditoko)
