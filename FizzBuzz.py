6e8d196d7f641e00aea73d2339246a02aa73cfe8
yuann468-lab <yuann468@gmail.com>
2026-09-09 09:35:34 +0700

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,14 @@
+n = int(input("masukan "))
+i = 1
+ 
+while i <=n:
+     if i % 3 == 0 and i % 5 == 0:
+         print("FizzBuzz")
+     elif i % 3 == 0:
+         print("Fizz")
+     elif i % 5 == 0:
+        print("Buzz")
+     else:
+        print(i)
+        
+     i += 1
